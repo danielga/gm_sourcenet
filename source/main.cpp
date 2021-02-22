3b3cbfca2522ee45c3609bc481c37d85d0902086
Daniel <danielga@outlook.pt>
2021-02-22 00:06:42 +0000

    Bumped revision number

--- source/main.cpp
+++ source/main.cpp
@@ -87,11 +87,11 @@ namespace global
 	{
 		LUA->CreateTable( );
 
-		LUA->PushString( "sourcenet 1.1.12" );
+		LUA->PushString( "sourcenet 1.1.13" );
 		LUA->SetField( -2, "Version" );
 
 		// version num follows LuaJIT style, xxyyzz
-		LUA->PushNumber( 10112 );
+		LUA->PushNumber( 10113 );
 		LUA->SetField( -2, "VersionNum" );
 
 		LUA->SetField( GarrysMod::Lua::INDEX_GLOBAL, "sourcenet" );
